f43d2b46458099e9f015eb0d134948ec5fe54abd
HKES-04 <husseinkhawalelsultan@gmail.com>
2023-06-22 18:29:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Dice Game
+
+This is a two player game where the users playing have to roll the dice, if the dice lands on 1 it automatically switches to the other player if it lands on a number other than 1 then the current score continues to add up as the user keeps rolling the dice. The user can decide to hold the score if they don't want to risk losing all the points they have saved up by landing on a 1, once the hold button is clicked it switches over to the other player. Whoever reaches the score of 50+ wins the game!
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+## Overview
+
+### Links
+
+- Solution URL: (https://github.com/HKES-04/Guess-The-Number.git)
+- Live Site URL: (https://hkes-04.github.io/Guess-The-Number/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScript
+
+### What I learned
+
+This game has provided an excellent opportunity for me to enhance my JavaScript skills while working seamlessly with HTML and CSS. By building this game, I have gained valuable experience in utilizing JavaScript to create interactive and dynamic elements and implementing game logic and mechanics. Furthermore, the integration of HTML and CSS has allowed me to design and style the game's user interface, providing an engaging and visually appealing experience for players. The combination of these technologies has not only expanded my knowledge but also enabled me to create a fully functional and enjoyable game.
---
+++ script.js
@@ -0,0 +1,87 @@
+"use strict";
+
+//Variables
+const score_0 = document.querySelector("#score--0");
+const score_1 = document.querySelector("#score--1");
+const dice = document.querySelector(".dice");
+const newButton = document.querySelector(".btn--new");
+const rollButton = document.querySelector(".btn--roll");
+const holdButton = document.querySelector(".btn--hold");
+const currentScore_0 = document.querySelector("#current--0");
+const currentScore_1 = document.querySelector("#current--1");
+const player_0 = document.querySelector(".player--0");
+const player_1 = document.querySelector(".player--1");
+
+// Initializing the values of variables
+let currentScore, currentlyPlaying, scores, activePlayer;
+resetIntialization();
+
+function switchPlayer() {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player_0.classList.toggle("player--active");
+  player_1.classList.toggle("player--active");
+}
+
+function resetIntialization() {
+  score_0.textContent = 0;
+  score_1.textContent = 0;
+  currentScore_0.textContent = 0;
+  currentScore_1.textContent = 0;
+  dice.classList.remove("hidden");
+  document;
+  player_0.classList.remove("player--winner");
+  player_1.classList.remove("player--winner");
+  player_0.classList.add("player--active");
+  player_1.classList.remove("player--active");
+  dice.classList.add("hidden");
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  currentlyPlaying = true;
+}
+
+// Rolling the dice
+rollButton.addEventListener("click", function () {
+  if (currentlyPlaying) {
+    // Generates a random number for the dice roll
+    const randomDice = Math.trunc(Math.random() * 6) + 1;
+    dice.classList.remove("hidden");
+    dice.src = `images/dice-${randomDice}.png`;
+
+    // Checks if the dice rolled is a 1
+    if (randomDice === 1) {
+      switchPlayer();
+    } else {
+      currentScore += randomDice;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore;
+    }
+  }
+});
+
+// Holding to save the current score
+holdButton.addEventListener("click", function () {
+  if (currentlyPlaying) {
+    scores[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // If a player has a score of 50+, they win!
+    if (scores[activePlayer] >= 50) {
+      currentlyPlaying = false;
+      dice.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// Resetting the game
+newButton.addEventListener("click", resetIntialization);
